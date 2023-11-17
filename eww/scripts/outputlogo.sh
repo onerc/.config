52c12ebc23fe755716614b25a3839d6c92d07893
onerc <94126150+onerc@users.noreply.github.com>
2023-11-17 09:51:18 +0000

    Create outputlogo.sh

---
+++ eww/scripts/outputlogo.sh
@@ -0,0 +1,17 @@
+while true; do
+activesink=$(pactl get-default-sink)
+if [[ $activesink =~ "hdmi" ]]; then
+        echo ""
+elif [[ $activesink =~ "analog" ]]; then
+        echo ""
+else
+        echo ""
+fi
+
+if [[ $status != $previous ]]; then
+     echo $status
+     previous=$status
+fi
+
+sleep 0.2
+done
