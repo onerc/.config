05b0bc59456fbc56afba6ee34f93945cff82d33e
onerc <94126150+onerc@users.noreply.github.com>
2023-11-20 19:54:03 +0000

    Update setvolume.sh

--- eww/scripts/setvolume.sh
+++ eww/scripts/setvolume.sh
@@ -1,7 +1,7 @@
 multipliedvolume="${2}0"
 case $1 in
     "sink")
-    pactl set-sink-volume @DEFAULT_SINK@ $multipliedvolume%;;
+        pactl set-sink-volume @DEFAULT_SINK@ $multipliedvolume%;;
     "source")
-    pactl set-source-volume @DEFAULT_SOURCE@ $multipliedvolume%;;
+        pactl set-source-volume @DEFAULT_SOURCE@ $multipliedvolume%;;
 esac
