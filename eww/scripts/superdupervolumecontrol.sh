c310a9bf418809375442dde8c069ee4fde16d10e
onerc <94126150+onerc@users.noreply.github.com>
2023-12-05 11:37:34 +0000

    Update superdupervolumecontrol.sh

--- eww/scripts/superdupervolumecontrol.sh
+++ eww/scripts/superdupervolumecontrol.sh
@@ -1,29 +1,14 @@
-increase_sink_volume() {
-    current_sink_volume=$(pactl get-sink-volume @DEFAULT_SINK@ | awk 'NR==1{print $5/1}')
-    if (( $current_sink_volume >= 90)); then
-        pactl set-sink-volume @DEFAULT_SINK@ 100%
-    else
-        pactl set-sink-volume @DEFAULT_SINK@ +10%
-    fi
-}
-
-increase_source_volume() {
-    current_source_volume=$(pactl get-source-volume @DEFAULT_SOURCE@ | awk 'NR==1{print $5/1}')
-    if (( $current_source_volume >= 90)); then
-        pactl set-source-volume @DEFAULT_SOURCE@ 100%
-    else
-        pactl set-source-volume @DEFAULT_SOURCE@ +10%
-    fi
-}
-
-
-case $1 in
-    sink-up)
-        increase_sink_volume;;
-    sink-down)
-        pactl set-sink-volume @DEFAULT_SINK@ -10%;;
-    source-up)
-        increase_source_volume;;
-    source-down)
-        pactl set-source-volume @DEFAULT_SOURCE@ -10%
+uppercased_arg=$(echo $1 | tr "[:lower:]" "[:upper:]")
+current_volume=$(pactl get-$1-volume @DEFAULT_$uppercased_arg@ | awk 'NR==1{print $5/1}')
+case $2 in
+    up)
+        if (( $current_volume >= 90)); then
+            pactl set-$1-volume @DEFAULT_$uppercased_arg@ 100%
+        else
+            pactl set-$1-volume @DEFAULT_$uppercased_arg@ +10%
+        fi
+        ;;
+    down)
+        pactl set-$1-volume @DEFAULT_$uppercased_arg@ -10%
+        ;;
 esac
