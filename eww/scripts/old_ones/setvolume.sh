85472774666e34c4c5d9956d2ffbb94fdc02ecba
onerc <94126150+onerc@users.noreply.github.com>
2024-03-26 13:00:26 +0000

    Delete eww/scripts/old_ones/setvolume.sh

--- eww/scripts/old_ones/setvolume.sh
+++
@@ -1,7 +0,0 @@
-multipliedvolume="${2}0"
-case $1 in
-    "sink")
-        pactl set-sink-volume @DEFAULT_SINK@ $multipliedvolume%;;
-    "source")
-        pactl set-source-volume @DEFAULT_SOURCE@ $multipliedvolume%;;
-esac
