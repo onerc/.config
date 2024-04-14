202b7341d76e83e04ef3a288cc517cabfda9f359
onerc <94126150+onerc@users.noreply.github.com>
2024-04-14 09:17:32 +0000

    Update config.js

--- ags/config.js
+++ ags/config.js
@@ -5,6 +5,7 @@ const { Button, Box, CenterBox, Icon, Label, Revealer, Window } = Widget
 
 const speakerRevealerState = Variable(false)
 const microphoneRevealerState = Variable(false)
+const cacheRevealerState = Variable(false)
 const isUnwantedSinkSelected = Variable(false)
 const unwantedSink = 'iec958'
 
@@ -18,14 +19,17 @@ const category = {
 
 const audioOutputSwitch = () => Button({
     on_clicked: () => Audio.speaker = Audio.speakers.find(sink => {return sink.stream !== Audio.speaker.stream}),
-    on_middle_click: () => console.log(Mpris.players),
-    child: Label('???').hook(Audio.speaker, self => {
+    on_middle_click: () => console.log(Mpris),
+    child: Icon().hook(Audio.speaker, self => {
         if (Audio.speaker.name?.includes('hdmi')) {
-            self.label = ""
+            self.icon = "video-display-symbolic"
+            self.size = 12
         } else if (Audio.speaker.name?.includes('analog')) {
-            self.label = ""
-        } else
-            self.label = ""
+            self.icon = "audio-headphones-symbolic"
+            self.size = 14
+        } 
+        // else
+        //     self.label = ""
     }),
 })
 
@@ -49,6 +53,23 @@ const Workspaces = () => Box({
 const Clock = () => Label({
     setup: self => self.poll(1000, self => Utils.execAsync(['date', '+%H:%M']).then(date => self.label = date)),
 })
+
+const cache = () => Button({
+    child: Box({
+        children: [
+            Icon({css: 'padding-left: 4px', icon: 'drive-removable-media-symbolic'}),
+            Revealer({
+                reveal_child: cacheRevealerState.bind(),
+                child: Label({css:'min-width: 2.6rem; padding: 0px 4px',
+                    setup: self => self.poll(1000, self => Utils.execAsync(['bash', '-c', "grep Dirty: /proc/meminfo | awk '{print $2$3}'"]).then(cacheinfo => self.label = cacheinfo))
+                }),
+                transition: 'slide_left',
+            })
+        ]
+    })
+})
+.on("enter-notify-event", () => cacheRevealerState.value = true)
+.on("leave-notify-event", () => cacheRevealerState.value = false)
 
 const nowPlaying = () => Button({
     on_clicked: () => Mpris.getPlayer()?.playPause(),
@@ -76,23 +97,24 @@ const speakerVolume = () => Button({
     on_scroll_down: () => {if (!(isUnwantedSinkSelected.value)) {Audio.speaker.volume -= 0.1}},
     on_clicked: () => Utils.execAsync(['pactl', 'set-sink-mute', '@DEFAULT_SINK@', 'toggle']),
 	child: Box({
-        css: 'padding: 3px',
         children: [
-            Icon().hook(Audio.speaker, self => {
+            Icon({css:'padding-left: 4px'}).hook(Audio.speaker, self => {
                 isUnwantedSinkSelected.value = Audio.speaker.name?.includes(unwantedSink)
                 const icon = Audio.speaker.stream?.is_muted || isUnwantedSinkSelected.value ? 0 : [99, 66, 33, 1, 0].find(threshold => threshold <= Audio.speaker.volume * 100)
                 self.icon = `audio-volume-${category[icon]}-symbolic`
             }),
             Revealer({
                 reveal_child: speakerRevealerState.bind(),
-                child: Label().hook(Audio.speaker, self => {self.label = isUnwantedSinkSelected.value ? ' N/A' : ` %${Math.round(Audio.speaker.volume * 100)}`}),
+                child: Label({css: 'min-width: 2.6rem; padding: 0px 4px'}).hook(Audio.speaker, self => {
+                    self.label = isUnwantedSinkSelected.value ? 'N/A' : `%${Math.round(Audio.speaker.volume * 100)}`
+                }),
                 transition: 'slide_left',
             }),
         ]
     })
 })
-.on("leave-notify-event", () => speakerRevealerState.value = false)
 .on("enter-notify-event", () => speakerRevealerState.value = true)
+.on("leave-notify-event", () => speakerRevealerState.value = false)
 
 
 const microphoneVolume = () => Button({
@@ -100,22 +122,23 @@ const microphoneVolume = () => Button({
     on_scroll_down: () => Audio.microphone.volume -= 0.1,
     on_clicked: () => Utils.execAsync(['pactl', 'set-source-mute', '@DEFAULT_SOURCE@', 'toggle']),
 	child: Box({
-        css: 'padding: 3px',
         children: [
-            Icon().hook(Audio.microphone, self => {
+            Icon({css:'padding-left: 4px'}).hook(Audio.microphone, self => {
                 const icon = Audio.microphone.stream?.is_muted ? 0 : [66, 33, 1, 0].find(threshold => threshold <= Audio.microphone.volume * 100)
                 self.icon = `microphone-sensitivity-${category[icon]}-symbolic`
             }),
             Revealer({
                 reveal_child: microphoneRevealerState.bind(),
-                child: Label().hook(Audio.microphone, self => {self.label = Audio.microphone.description == null ? ' N/A' : ` %${Math.round(Audio.microphone.volume * 100)}`}),
+                child: Label({css: 'min-width: 2.6rem; padding: 0px 4px'}).hook(Audio.microphone, self => {
+                    self.label = Audio.microphone.description == null ? 'N/A' : `%${Math.round(Audio.microphone.volume * 100)}`
+                }),
                 transition: 'slide_left',
             }),
         ]
     })
 })
-.on("leave-notify-event", () => microphoneRevealerState.value = false)
 .on("enter-notify-event", () => microphoneRevealerState.value = true)
+.on("leave-notify-event", () => microphoneRevealerState.value = false)
 
 
 
@@ -138,6 +161,7 @@ const Right = () => Box({
     hpack: 'end',
     //spacing: 8,
     children: [
+        cache(),
         microphoneVolume(),
         speakerVolume(),
         audioOutputSwitch(),
@@ -154,7 +178,7 @@ const Bar = (monitor = 0) => Window({
         start_widget: Left(),
         center_widget: Center(),
         end_widget: CenterBox({
-            center_widget: nowPlaying(), 
+            center_widget: nowPlaying(),
             end_widget: Right()
         }),
     }),
