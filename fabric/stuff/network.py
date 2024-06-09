4d82b69f363f4363302ea1e8a0b7a4ea9244d3d7
onerc <94126150+onerc@users.noreply.github.com>
2024-06-09 11:17:07 +0000

    Update network.py

--- fabric/stuff/network.py
+++ fabric/stuff/network.py
@@ -1,14 +1,7 @@
-import psutil
-
-from fabric.widgets.box import Box
-from fabric.widgets.button import Button
-from fabric.widgets.image import Image
-from fabric.widgets.label import Label
-from fabric.widgets.revealer import Revealer
-
+from fabric.widgets import Box, Button, Image, Label, Revealer, Stack
 from fabric.utils import invoke_repeater
-
 from gi.repository import Gtk
+import psutil
 
 network_interface = "enp6s0"
 
@@ -18,7 +11,11 @@ class Network(Button):
         self.is_pinned = False
 
         self.label = Label(name="revealerLabel")
-        self.button_icon = Image(name="revealerIcon", icon_name="network-wired-acquiring-symbolic", icon_size=Gtk.IconSize(1))
+
+        self.stack = Stack(transition_type="slide-up-down")
+        for i in ["network-wired-acquiring", "network-wired", "network-wired-disconnected", "lock"]:
+            self.stack.add_named(Image(icon_name=f"{i}-symbolic", icon_size=Gtk.IconSize(1), name="revealerIcon"), name=i)
+
         self.revealer = Revealer(
             children=self.label,
             transition_type="slide-left"
@@ -29,7 +26,7 @@ class Network(Button):
             on_leave_notify_event=lambda *args: self.revealer.set_reveal_child(False) if not self.is_pinned else None,
             child=Box(
                 children=[
-                    self.button_icon,
+                    self.stack,
                     self.revealer
                 ]
             )
@@ -39,16 +36,17 @@ class Network(Button):
 
     def toggle_pin(self, *args):
         self.is_pinned = not self.is_pinned
-        self.button_icon.set_from_icon_name("lock-symbolic" if self.is_pinned else self.find_icon(), Gtk.IconSize(1))
+        self.stack.set_visible_child_name("lock" if self.is_pinned else self.find_icon())
 
     # todo bandwidth usage
     # maybe todo interface speed
+
     def update_label_and_icon(self, *args):
         self.label.set_label(psutil.net_if_addrs()[network_interface][0].address if psutil.net_if_stats()[network_interface].isup else "N/A")
         if not self.is_pinned:
-            self.button_icon.set_from_icon_name(self.find_icon(), Gtk.IconSize(1))
+            self.stack.set_visible_child_name(self.find_icon())
         return True
 
     @staticmethod
     def find_icon():
-        return "network-wired-symbolic" if psutil.net_if_stats()[network_interface].isup else "network-wired-disconnected-symbolic"
+        return "network-wired" if psutil.net_if_stats()[network_interface].isup else "network-wired-disconnected"
