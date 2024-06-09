db14d19955833aa5881b6be4db2c4702b7b519a8
onerc <94126150+onerc@users.noreply.github.com>
2024-06-09 11:16:04 +0000

    Update cache.py

--- fabric/stuff/cache.py
+++ fabric/stuff/cache.py
@@ -1,11 +1,5 @@
-from fabric.widgets.box import Box
-from fabric.widgets.button import Button
-from fabric.widgets.image import Image
-from fabric.widgets.label import Label
-from fabric.widgets.revealer import Revealer
-
+from fabric.widgets import Box, Button, Image, Label, Revealer, Stack
 from fabric.utils.fabricator import Fabricator
-
 from gi.repository import Gtk
 
 dirty_fabricator = Fabricator(poll_from="grep Dirty: /proc/meminfo", interval=1000)
@@ -16,7 +10,11 @@ class Cache(Button):
         self.is_pinned = False
 
         self.label = Label(name="cacheLabel")
-        self.button_icon = Image(name="revealerIcon", icon_name="drive-removable-media-symbolic", icon_size=Gtk.IconSize(1))
+
+        self.stack = Stack("slide-up-down")
+        for i in ["drive-removable-media", "lock"]:
+            self.stack.add_named(Image(name="revealerIcon", icon_name=f"{i}-symbolic", icon_size=Gtk.IconSize(1)), name=i)
+
         self.revealer = Revealer(
             children=self.label,
             transition_type="slide-left"
@@ -27,7 +25,7 @@ class Cache(Button):
             on_leave_notify_event=lambda *args: self.revealer.set_reveal_child(False) if not self.is_pinned else None,
             child=Box(
                 children=[
-                    self.button_icon,
+                    self.stack,
                     self.revealer
                 ]
             )
@@ -37,15 +35,17 @@ class Cache(Button):
 
     def toggle_pin(self, *args):
         self.is_pinned = not self.is_pinned
-        self.button_icon.set_from_icon_name("lock-symbolic" if self.is_pinned else "drive-removable-media-symbolic", Gtk.IconSize(1))
+        self.stack.set_visible_child_name("lock" if self.is_pinned else "drive-removable-media")
+
+    @staticmethod
+    def find_label(value):
+        cache = int(value.split()[1])
+        return (
+            f"{round(cache / 1048576, 1)} GB" if cache >= 1048576
+            else f"{round(cache / 1024, 1)} MB" if cache >= 1024
+            else f"{cache} KB"
+        )
 
     def update_label(self, fabricator, value):
-        cache = int(value.split()[1])
-        if cache >= 1048576:
-            cache = f"{round(cache / 1048576, 1)} GB"
-        elif cache >= 1024:
-            cache = f"{round(cache / 1024, 1)} MB"
-        else:
-            cache = f"{cache} KB"
-        self.label.set_label(cache)
+        self.label.set_label(self.find_label(value))
         return True
